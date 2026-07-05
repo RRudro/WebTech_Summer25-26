ed15f8353c336669e447c55995416424af4070b2
RRudro <mamun.rudro@aiub.edu>
2026-07-05 19:24:34 +0600

    Add User Registration page: Create HTML structure, link CSS and JavaScript, and implement form fields

---
+++ WebTech_T/Theory(HTML_CSS_JS_1)/JavaScript/Script.js
@@ -0,0 +1,16 @@
+console.log("Hello World");
+// alert("Hello JavaScript");
+
+let a=10;
+var b=20; 
+
+let sum= a+b;
+console.log(sum);
+
+var name ="AIUB";
+var name ='DEPT CS';
+console.log(name);
+
+var text ="AASDFGHJKLWERTYUIOERTYUI";
+var str = text.length;
+console.log(str); 
