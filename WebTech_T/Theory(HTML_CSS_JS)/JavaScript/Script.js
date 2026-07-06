dd1f3af202ad7c68cca699f3c09da8d1232cca0f
RRudro <mamun.rudro@aiub.edu>
2026-07-06 09:25:24 +0600

    Implement data collection on form submission: Update User.html to call collect_data function and enhance Script.js with data logging functionality

--- WebTech_T/Theory(HTML_CSS_JS)/JavaScript/Script.js
+++ WebTech_T/Theory(HTML_CSS_JS)/JavaScript/Script.js
@@ -1,8 +1,30 @@
-console.log("Hello World");
-alert("Hello JavaScript");
+console.log("HTML Connected");
+// let a=10.50;
+// var b=20;
+// sum=a+b;
+// sub=a-b
+// x=a+b;
+// console.log(sum);
+// console.log(sub);
+// console.log(x);
 
-let a=10;
-var b=20; 
+// let name="AIUB";
+// name='BUET';
+// console.log(name);
 
-let sum= a+b;
-console.log(sum);
+// a="ASDFGHJKLWERTYUIO";
+// var text=a.length;
+// console.log(text);
+function collect_data()
+{
+    let pname=document.getElementById("name").value;
+    console.log(pname);
+    let collectDOB = collect_DOB();
+    return false;
+}
+function collect_DOB()
+{
+    let dob=document.getElementById("DOB").value;
+    console.log(dob);
+    return false;
+}
