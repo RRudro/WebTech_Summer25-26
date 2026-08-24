2d3d18c5cf9825f13d98cda2aa0c5e1c2507e9b7
RRudro <mamun.rudro@aiub.edu>
2026-08-24 09:34:17 +0600

    Fix username input retrieval and response handling in CheckUser function

--- WebTech_T/Final Term/Theory4 (AJAX)/JS/CheckUser.js
+++ WebTech_T/Final Term/Theory4 (AJAX)/JS/CheckUser.js
@@ -1,7 +1,7 @@
 function CheckUser()
 {
-    let username=document.getElementById("name").value.trim();
-    let reponse= document.getElementById("userresponse");
+    let username=document.getElementById("username").value.trim();
+    let response= document.getElementById("userresponse");
     let xhttp= new XMLHttpRequest();
     xhttp.onreadystatechange=function(){
         if(this.readyState==4 && this.status==200)
@@ -14,6 +14,6 @@ function CheckUser()
     }
     xhttp.open("POST", "../Controller/CheckUser.php", true);
     xhttp.setRequestHeader("content-type", "application/x-www-form-urlencoded");
-    xhttp.send("username" + username);
+    xhttp.send("username=" +encodeURIComponent(username));
 
 }
